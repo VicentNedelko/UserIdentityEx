c0b63559f1939ab2262aae005aa8173ba2263391
Vicent Niad <glory.jly@gmail.com>
2021-01-26 16:04:17 +0300

    Add project files.

---
+++ UserIdentityEx/Controllers/AccountController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using UserIdentityEx.Models;
+using UserIdentityEx.ViewModels;
+
+namespace UserIdentityEx.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<User> _usermanager;
+        private readonly SignInManager<User> _signInManager;
+        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            _usermanager = userManager;
+            _signInManager = signInManager;
+        }
+        [HttpGet]
+        public async void Edit(User user)
+        {
+            await _usermanager.DeleteAsync(user);
+        }
+        [HttpGet]
+        public async Task<IActionResult> ShowList()
+        {
+            return View(await _usermanager.Users.ToListAsync());
+        }
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Register(UserViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = new User
+                {
+                    Age = model.Age,
+                    Email = model.Email,
+                    UserName = model.Name,
+                    PasswordHash = model.Password,
+                    Code = model.Code,
+                };
+                var result = await _usermanager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    await _signInManager.SignInAsync(user, false);
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                }
+            }
+            return View(model);
+        }
+    }
+}
---
+++ UserIdentityEx/ViewModels/UserViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using UserIdentityEx.Models;
+
+namespace UserIdentityEx.ViewModels
+{
+    public class UserViewModel
+    {
+        [Required(ErrorMessage = "Email required.")]
+        [Display(Name = "Email")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Age required.")]
+        [Display(Name = "Age")]
+        [Range(10, 100)]
+        public int Age { get; set; }
+
+        [Required]
+        [Display(Name = "Code")]
+        public string Code { get; set; }
+
+        [Required]
+        [Display(Name = "Pass")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [Display(Name = "Pass Confirm")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Passes are not equal.")]
+        public string PasswordConfirm { get; set; }
+    }
+}
